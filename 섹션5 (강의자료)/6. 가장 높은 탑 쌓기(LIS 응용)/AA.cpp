27cae7848c62ae34e64e9f9cca5d114f6106184f
pqj163 <58128948+pqj163@users.noreply.github.com>
2021-02-18 21:38:26 +0900

    dy

--- 섹션5 (강의자료)/6. 가장 높은 탑 쌓기(LIS 응용)/AA.cpp
+++ 섹션5 (강의자료)/6. 가장 높은 탑 쌓기(LIS 응용)/AA.cpp
@@ -1,26 +1,46 @@
+#include <algorithm>
 #include <stdio.h>
+#include <vector>
+
+struct Brick
+{
+    int s, h, w;
+    Brick(int a, int b, int c)
+    {
+        s = a, h = b, w = c;
+    }
+    bool operator<(const Brick &b) const
+    {
+        return s > b.s;
+    }
+};
 
 int main()
 {
-    int n, res;
+    int n, a, b, c, max_h = 0, res = 0;
     scanf("%d", &n);
-    int arr[n + 1], dy[n + 1];
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &arr[i]);
-    dy[1] = 1;
-    for (int i = 2; i <= n; i++)
+    std::vector<Brick> Bricks;
+    std::vector<int> dy(n, 0);
+    int met[n + 1][3];
+    for (int i = 0; i < n; i++)
     {
-        int max = 0;
-        for (int j = i - 1; j >= 1; j--)
-        {
-            if (arr[j] < arr[i] && dy[j] > max)
-                max = dy[j];
-        }
-        // 사실상 전의 코드를 그대로 베꼈는데, 솔직히 왜 여기서 +1 하는 건지 여전히 잘 모르겠다. 뭔가 조건문이 있어야 하지 않나?
-        dy[i] = max + 1;
-        if (dy[i] > res)
-            res = dy[i];
+        scanf("%d %d %d", &a, &b, &c);
+        Bricks.push_back(Brick(a, b, c));
     }
-    printf("%d\n", res);
+    sort(Bricks.begin(), Bricks.end());
+    dy[0] = Bricks[0].h;
+    res = dy[0];
+    for (int i = 1; i < n; i++)
+    {
+        max_h = 0;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (Bricks[j].w > Bricks[i].w && dy[j] > max_h)
+                max_h = dy[j];
+        }
+        dy[i] = max_h + Bricks[i].h;
+        res = dy[i] > res ? dy[i] : res;
+    }
+    printf("%d", res);
     return 0;
 }
